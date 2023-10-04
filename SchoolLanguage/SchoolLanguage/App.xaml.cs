9aef85040c644e783619c88303ff0f292b6eef50
Kakashi <laysandattebayo99@gmail.com>
2023-10-04 15:08:07 +0300

    доделать

--- SchoolLanguage/SchoolLanguage/App.xaml.cs
+++ SchoolLanguage/SchoolLanguage/App.xaml.cs
@@ -13,5 +13,6 @@ namespace SchoolLanguage
     /// </summary>
     public partial class App : Application
     {
+        public static Components.SchoolLanguageEntities db = new Components.SchoolLanguageEntities();
     }
 }
--- SchoolLanguage/SchoolLanguage/MainWindow.xaml.cs
+++ SchoolLanguage/SchoolLanguage/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,14 @@ namespace SchoolLanguage
         public MainWindow()
         {
             InitializeComponent();
+            var path = "@C:\\Users\\222121\\AppData\\Local\\Temp\\Tempeb4bc23e-20d9-4d3c-aed0-39f29236dc2b_Task.zip\\Сессия 1\\services_s_import.zip\\Услуги школы";
+            foreach(var item in App.db.Service.ToArray())
+            {
+                var fullPath = path + item.MainImagePage;
+                var imageByte = File.ReadAllBytes(fullPath);
+                item.MainImagePage = imageByte;
+            }
+            App.db.SaveChanges();
         }
     }
 }
