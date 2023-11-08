35e43beafb306c2f61e3d7356fec6b8c636ce9fa
Kakashi <laysandattebayo99@gmail.com>
2023-11-08 13:10:41 +0300

    fghgf

--- SchoolLanguage/SchoolLanguage/Components/Clients.cs
+++ SchoolLanguage/SchoolLanguage/Components/Clients.cs
@@ -9,10 +9,11 @@ namespace SchoolLanguage.Components
     public partial class Clients
     {
         public string FullName
-            { 
+        {
             get
             {
                 return $"{FirstName} {LastName} {Patronomic}";
             }
+        }
     }
 }
--- SchoolLanguage/SchoolLanguage/Pages/ClientRecordPage.xaml.cs
+++ SchoolLanguage/SchoolLanguage/Pages/ClientRecordPage.xaml.cs
@@ -32,5 +32,51 @@ namespace SchoolLanguage.Pages
             ClientCb.DisplayMemberPath = "FullName";
             DateDp.DisplayDateStart = DateTime.Today;
         }
+        private void EntryBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(ClientCb.SelectedItem != null && DateDp.SelectedDate != null && StartTimeTb.Text != "")
+            {
+                var startDate = $"{DateDp.SelectedDate.Value.ToString("dd.MM.yyyy")} {StartTimeTb.Text}";
+                if(DateTime.TryParse(startDate, out DateTime dateTimeStart))
+                {
+                    if(dateTimeStart > DateTime.Now)
+                    {
+                        var selectTClient = ClientCb.SelectedItem as Client;
+                        App.db.ClientService.Add(new ClientService()
+                        {
+                            ClientID = selectTClient.ID,
+                            ServiceID = service.ID,
+                            StartTime = dateTimeStart,
+                        });
+                        App.db.SaveChanges();
+                        MessageBox.Show("Запись добавлена!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Время прошло!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Время введено неверно!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Заполните все поля!");
+            }
+        }
+        private void StartTimeTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var time = StartTimeTb.Text.Split(':');
+            var hour = int.Parse(time[0]);
+            var minute = int.Parse(time[1]);
+            TimeSpan timeStart = new TimeSpan(hour, minute, 0);
+            TimeSpan timeJob = new TimeSpan(service.DurationInSeconds/3600, service.DurationInSeconds % 3600, 0);
+            if (StartTimeTb.Text.Length == 5)
+            {
+                EndTimeTb.Text = (timeStart + timeJob).ToString();
+            }
+        }
     }
 }
